71c318c1a9b4e59d7f2d3809f0075e548492790c
erichstuder <richu_@hotmail.com>
2020-07-05 00:19:56 +0200

    correct import path

--- Client.py
+++ Client.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from app.ComportHandler import ComportHandler
+from helpers.ComportHandler import ComportHandler
 import threading
 import time
 import os
