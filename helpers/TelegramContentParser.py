cac8ea6bd673a672bb8c2dc550abc75192fcae96
erichstuder <richu_@hotmail.com>
2021-06-20 13:46:08 +0200

    initial commit

---
+++ helpers/TelegramContentParser.py
@@ -0,0 +1,106 @@
+"""
+IT - Internal Tracer
+Copyright (C) 2019 Erich Studer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+class TelegramContentParserException(Exception):
+	pass
+
+class _TelegramContentParser:
+
+	@staticmethod
+	def parseTelegramType(telegram, telegramStream):
+		telegramTypes = {
+			1: 'value',
+			2: 'string',
+		}
+		if len(telegramStream) < 1:
+			raise TelegramContentParserException('stream is empty')
+		telegramType = telegramTypes.get(telegramStream[0])
+		if telegramType == None:
+			raise TelegramContentParserException('invalid telegram type')
+		telegram['telegramType'] = telegramType
+		newTelegramStream = telegramStream[1:]
+		return newTelegramStream
+
+	@staticmethod
+	def parseValueName(telegram, telegramStream):
+		name = ''
+		nameLength = 0
+		for byte in telegramStream:
+			if byte == 0:
+				if len(name) < 2:
+					raise TelegramContentParserException('string is empty')
+				telegram['valueName'] = name
+				return telegramStream[nameLength+1:]
+			name += chr(byte)
+			nameLength += 1
+		raise TelegramContentParserException('value name has no terminator')
+
+	@staticmethod
+	def parseValueType(telegram, telegramStream):
+		valueTypes = {
+			1: 'int8',
+			2: 'uint8',
+			3: 'ulong',
+			4: 'float',
+		}
+		if len(telegramStream) == 0:
+			raise ValueError('no value to parse')
+		valueType = valueTypes.get(telegramStream[0])
+		if valueType == None:
+			raise ValueError('invalid value type')
+		telegram['valueType'] = valueType
+		return telegramStream[1:]
+
+	@staticmethod
+	def parseValue(telegram, telegramStream):
+		if telegram['valueType'] == 'uint8':
+			size = 1
+			if len(telegramStream) < size:
+				raise ValueError('not enough bytes to parse uint8')
+			telegram['value'] = struct.unpack('B', bytes(telegramStream[:size]))[0]
+		elif telegram['valueType'] == 'ulong':
+			size = 4
+			if len(telegramStream) < size:
+				raise ValueError('not enough bytes to parse ulong')
+			telegram['value'] = struct.unpack('L', bytes(telegramStream[:size]))[0]
+		elif telegram['valueType'] == 'float':
+			size = 4
+			if len(telegramStream) < size:
+				raise ValueError('not enough bytes to parse float')
+			telegram['value'] = struct.unpack('f', bytes(telegramStream[:size]))[0]
+		return telegramStream[size:]
+
+	@staticmethod
+	def parseTimestamp(telegram, telegramStream):
+		size = 4
+		if len(telegramStream) < size:
+			raise ValueError('not enough bytes to parse timestamp')
+		telegram['timestamp'] = struct.unpack('L', bytes(telegramStream[:size]))[0]
+		return telegramStream[size:]
+
+	@staticmethod
+	def parseString(telegram, telegramStream):
+		name = ''
+		nameLength = 0
+		for byte in telegramStream:
+			if byte == 0:
+				telegram['value'] = name
+				return telegramStream[nameLength + 1:]
+			name += chr(byte)
+			nameLength += 1
+		raise ValueError('string has no terminator')
---
+++ tests/TelegramContentParser_test.py
@@ -0,0 +1,61 @@
+"""
+IT - Internal Tracer
+Copyright (C) 2019 Erich Studer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import pytest
+from helpers.TelegramContentParser import _TelegramContentParser
+from helpers.TelegramContentParser import TelegramContentParserException
+
+def test_parseTelegramType_emptyStream():
+	with pytest.raises(TelegramContentParserException, match='stream is empty'):
+		_TelegramContentParser.parseTelegramType(None, [])
+
+
+@pytest.mark.parametrize('telegramType', [(0x00), (0x03)])
+def test_parseTelegramType_invalidType(telegramType):
+	with pytest.raises(TelegramContentParserException, match='invalid telegram type'):
+		_TelegramContentParser.parseTelegramType(None, [telegramType])
+
+
+@pytest.mark.parametrize('telegramType, telegramTypeString',
+			 [(0x01,       'value'),
+			  (0x02,       'string'),
+			 ])
+def test_parseTelegramType(telegramType, telegramTypeString): 	
+	rest = 0x42
+	telegram = {}
+	newStream = _TelegramContentParser.parseTelegramType(telegram, [telegramType, rest])
+	assert newStream == [rest]
+	assert telegram['telegramType'] == telegramTypeString
+
+
+def test_parseValueName_emptyStream():
+	with pytest.raises(TelegramContentParserException, match='value name has no terminator'):
+		_TelegramContentParser.parseValueName(None, [])
+
+def test_parseValueName_emptyString():
+	with pytest.raises(TelegramContentParserException, match='string is empty'):
+		_TelegramContentParser.parseValueName({}, [0x00])
+
+def test_parseValueName():
+	telegram = {}
+	stream = [ord(c) for c in 'Hello'] + [0x00, 11, 22, 33]
+	newStream = _TelegramContentParser.parseValueName(telegram, stream)
+	assert telegram['valueName'] == 'Hello'
+	assert newStream == [11, 22, 33]
+
+
