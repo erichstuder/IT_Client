105362b8dd6b330c1b373441d6ec502cd79c3e90
erichstuder <richu_@hotmail.com>
2021-08-21 12:33:32 +0200

    get telegram range at the end

--- helpers/TelegramParser.py
+++ helpers/TelegramParser.py
@@ -22,75 +22,24 @@ class TelegramParser:
 	def __init__(self, sessionFilePath):
 		self.__telegramReader = _TelegramReader(sessionFilePath)
 
-	def getLastValueByName(self, name):
+
+	def getLastValue(self, name):
 		for telegram in reversed(self.__telegramReader.getTelegrams()):
 			if telegram['valid'] == True and telegram['telegramType'] == 'value' and telegram['valueName'] == name:
 				return telegram
 		return None
 
 
-	@staticmethod
-	def parseLastValidTypeValueTelegram(data: bytes, valueName):
-		lastDataIndex = len(data)-1
-		endIndex = lastDataIndex
-		for index in range(lastDataIndex, 0, -1):
-			byte = data[index]
-			if byte == TelegramParser.__TelegramEndId:
-				endIndex = index
-			elif byte == TelegramParser.__TelegramStartId:
-				startIndex = index
-				telegrams = TelegramParser.__splitTelegramStream(data[startIndex:endIndex+1])
-				TelegramParser.__parse(telegrams)
-				telegram = telegrams[0]
-				if telegram["valid"] == True and telegram["telegramType"] == "value" and telegram["valueName"] == valueName:
-					return telegram
-		return None
-
-
-	@staticmethod
-	def getTypeValueTelegramsAfterTimestamp(data: bytes, valueName, lowestTimestamp):
+	def getLastValues(self, name, timestampRange):
 		telegrams = []
-		lastDataIndex = len(data)-1
-		endIndex = lastDataIndex
-		for index in range(lastDataIndex, 0, -1):
-			byte = data[index]
-			if byte == TelegramParser.__TelegramEndId:
-				endIndex = index
-			elif byte == TelegramParser.__TelegramStartId:
-				startIndex = index
-				telegramsTemp = TelegramParser.__splitTelegramStream(data[startIndex:endIndex+1])
-				TelegramParser.__parse(telegramsTemp)
-				telegram = telegramsTemp[0]
-				if telegram["valid"] and telegram["telegramType"] == "value":
-					if telegram["timestamp"] <= lowestTimestamp:
-						break
-					elif telegram["valueName"] == valueName:
-						telegrams.insert(0, telegram)
+		lastTelegram = self.getLastValue(name)
+		if lastTelegram is None:
+			return None
+		lastTimestamp = lastTelegram['timestamp']
+		for telegram in reversed(self.__telegramReader.getTelegrams()):
+			if telegram['valid'] == True:
+				if telegram['timestamp'] < lastTimestamp - timestampRange:
+					return telegrams
+				if telegram['telegramType'] == 'value' and telegram['valueName'] == name:
+					telegrams = [telegram] + telegrams
 		return telegrams
-
-
-	@classmethod
-	def parseStream(cls, data: bytes):
-		telegrams = _TelegramFrameParser.splitIntoTelegrams(data)
-		for telegram in telegrams:
-			cls.__parse(telegram)
-		return telegrams
-
-
-	@staticmethod
-	def __parse(telegram):
-		try:
-			contentStream = _TelegramFrameParser.extractContent(telegram['raw'])
-			streamNoTelegramType = _TelegramContentParser.parseTelegramType(telegram, contentStream)
-			if telegram['telegramType'] == 'value':
-				streamNoValueName = _TelegramContentParser.parseValueName(telegram, streamNoTelegramType)
-				streamNoValueType = _TelegramContentParser.parseValueType(telegram, streamNoValueName)
-				streamNoValue = _TelegramContentParser.parseValue(telegram, streamNoValueType)
-				_TelegramContentParser.parseTimestamp(telegram, streamNoValue)
-			elif telegram['telegramType'] == 'string':
-				_TelegramContentParser.parseString(telegram, streamNoTelegramType)
-			else:
-				raise ValueError('unknown telegram type')
-			telegram["valid"] = True
-		except TelegramFrameParserException or TelegramContentParserException as e:
-			telegram["valid"] = False
