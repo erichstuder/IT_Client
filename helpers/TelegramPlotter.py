7a88a968f58ad382ba4092395f43e815ef558d44
erichstuder <richu_@hotmail.com>
2021-11-14 08:42:45 +0100

    use Process to plot
    For simpler use a process now handles the plotting.
    This makes it easier to have responsive plots.

--- helpers/TelegramPlotter.py
+++ helpers/TelegramPlotter.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import matplotlib.pyplot as plt
+from multiprocessing import Process, Queue
 
 class TelegramPlotterException(Exception):
 	pass
@@ -25,53 +25,72 @@ class TelegramPlotterException(Exception):
 class TelegramPlotter:
 	def __init__(self, title):
 		self.__plotTitle = title
-		plt.ion()
-		plt.style.use('dark_background')
 		self.__plotData = {}
 		self.__id = 0
-		self.__plotNumber = 1
+		self.__queue = Queue(maxsize=1)
+		plotterProcess = Process(target=self.__plotterProcess, daemon=True)
+		plotterProcess.start()
 
 
-	def plot(self, *telegramsList):
-		for telegrams in telegramsList:
-			name = telegrams[0]['valueName']
-			if name not in self.__plotData:
-				self.__plotData[self.__id] = {}
-			value = []
-			timestamp = []
-			for telegram in telegrams:
-				if telegram['valueName'] != name:
-					raise TelegramPlotterException('Telegrams must all have the same name.')
-				if telegram['valid'] != True:
-					raise TelegramPlotterException('Telegrams must all be valid.')
-				value += [telegram['value']]
-				timestamp += [telegram['timestamp']]
-			self.__plotData[self.__id]['value'] = value
-			self.__plotData[self.__id]['timestamp'] = timestamp
-			self.__plotData[self.__id]['plotNumber'] = self.__plotNumber
-			self.__plotData[self.__id]['name'] = name
-			self.__id += 1
-		self.__plotNumber += 1
+	def __plotterProcess(self):
+
+		# This import prevents a startup delay and an error message
+		# The hint came from here: https://github.com/matplotlib/matplotlib/issues/21627
+		# Not sure why this works
+		import matplotlib.pyplot as plt
+
+		plt.style.use('dark_background')
+		numberOfPlots_old = -1
+		while True:
+			if not self.__queue.empty():
+				plotData = self.__queue.get()
+				plotNumber = 0
+				legendString = []
+				numberOfPlots = len(plotData)
+				if numberOfPlots < 1:
+					continue
+				if numberOfPlots != numberOfPlots_old:
+					figure, axes = plt.subplots(numberOfPlots, 1, sharex=True, figsize=(12, 6))
+					figure.suptitle(self.__plotTitle)
+					numberOfPlots_old = numberOfPlots
+				for key in plotData:
+					axis = axes[plotNumber]
+					axis.clear()
+					axis.ticklabel_format(useOffset=False)
+					axis.grid(color='grey')
+					value = plotData[key]['value']
+					timestamp = plotData[key]['timestamp']
+					axis.step(timestamp, value, where='post')
+					legendString = plotData[key]['name']
+					axis.legend([legendString], loc='lower left')
+					plotNumber += 1
+				plt.show(block=False)
+			plt.pause(0.001)
+
+
+	def plot(self, telegrams):
+		if len(telegrams) < 1:
+			return
+		name = telegrams[0]['valueName']
+
+		if name not in self.__plotData:
+			self.__plotData[self.__id] = {}
+		value = []
+		timestamp = []
+		for telegram in telegrams:
+			if telegram['valueName'] != name:
+				raise TelegramPlotterException('Telegrams must all have the same name.')
+			if telegram['valid'] != True:
+				raise TelegramPlotterException('Telegrams must all be valid.')
+			value += [telegram['value']]
+			timestamp += [telegram['timestamp']]
+		self.__plotData[self.__id]['value'] = value
+		self.__plotData[self.__id]['timestamp'] = timestamp
+		self.__plotData[self.__id]['name'] = name
+		self.__id += 1
 
 
 	def update(self):
-		figure = plt.figure(num=self.__plotTitle, figsize=(8, 4))
-		plt.clf()
-		plotNumber = 0
-		legendStrings = []
-		for key in self.__plotData:
-			value = self.__plotData[key]['value']
-			timestamp = self.__plotData[key]['timestamp']
-			if plotNumber != self.__plotData[key]['plotNumber']:
-				plotNumber = self.__plotData[key]['plotNumber']
-				plt.subplot(self.__plotNumber-1, 1, plotNumber)
-				plt.ticklabel_format(useOffset=False)
-				legendStrings = []
-			plt.grid(color='grey')
-			plt.step(timestamp, value, where='post')
-			legendStrings.append(self.__plotData[key]['name'])
-			plt.legend(legendStrings, loc='lower left')
-		figure.canvas.flush_events()
+		self.__queue.put(self.__plotData)
 		self.__plotData = {}
 		self.__id = 0
-		self.__plotNumber = 1
